8ae26310d29794994b9f929944be3f055df6718d
Abien Fred Agarap <abien_agarap@dlsu.edu.ph>
2019-11-01 20:15:59 +0800

    refactor: write Encoder and Decoder classes

--- models/torch/autoencoder.py
+++ models/torch/autoencoder.py
@@ -10,24 +10,45 @@ torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
 
-class AE(nn.Module):
+class Encoder(nn.Module):
     def __init__(self):
         super().__init__()
         self.encoder_hidden_layer = nn.Linear(784, 128)
         self.encoder_output_layer = nn.Linear(128, 128)
-        self.decoder_hidden_layer = nn.Linear(128, 128)
-        self.decoder_output_layer = nn.Linear(128, 784)
 
     def forward(self, features):
         activation = self.encoder_hidden_layer(features)
         activation = F.relu(activation)
         code = self.encoder_output_layer(activation)
         code = F.relu(code)
-        activation = self.decoder_hidden_layer(code)
+        return code
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.decoder_hidden_layer = nn.Linear(128, 128)
+        self.decoder_output_layer = nn.Linear(128, 784)
+
+    def forward(self, features):
+        activation = self.decoder_hidden_layer(features)
         activation = F.relu(activation)
-        reconstructed = self.decoder_output_layer(activation)
-        reconstructed = F.relu(reconstructed)
+        activation = self.decoder_output_layer(activation)
+        reconstructed = F.relu(activation)
         return reconstructed
+
+
+class AE(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.encoder = Encoder()
+        self.decoder = Decoder()
+
+    def forward(self, features):
+        code = self.encoder(features)
+        reconstructed = self.decoder(code)
+        return reconstructed
+
 
 transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
 
