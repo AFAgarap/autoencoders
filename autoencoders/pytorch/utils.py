d25313f4d18f4976706cdda4851c54fd86c28528
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-02-26 22:58:59 +0800

    feat: add load_dataset()
    This function loads the MNIST dataset from `torchvision`.

--- autoencoders/pytorch/utils.py
+++ autoencoders/pytorch/utils.py
@@ -3,10 +3,23 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+from typing import Tuple
+
 import torch
+import torchvision
 
 __author__ = "Abien Fred Agarap"
 __version__ = "1.0.0"
+
+
+def load_dataset() -> Tuple[object, object]:
+    train_dataset = torchvision.datasets.MNIST(
+            root="~/torch_datasets", train=True, download=True
+            )
+    test_dataset = torchvision.datasets.MNIST(
+            root="~/torch_datasets", train=False, download=True
+            )
+    return (train_dataset, test_dataset)
 
 
 def create_dataloader(
