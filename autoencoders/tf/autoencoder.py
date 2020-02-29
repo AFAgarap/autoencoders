4028695c2ceed8a646db8dcc74837b494a62c6a2
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-03-01 01:11:16 +0800

    docs: add header

--- autoencoders/tf/autoencoder.py
+++ autoencoders/tf/autoencoder.py
@@ -1,4 +1,34 @@
-"""Implementation of vanila autoencoder in TensorFlow 2.0 Subclassing API"""
+# A Tutorial on Autoencoders
+# Copyright (C) 2020  Abien Fred Agarap and Richard Ralph Ricardo
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Also add information on how to contact you by electronic and paper mail.
+#
+# If your software can interact with users remotely through a computer
+# network, you should also make sure that it provides a way for users to
+# get its source.  For example, if your program is a web application, its
+# interface could display a "Source" link that leads users to an archive
+# of the code.  There are many ways you could offer source, and different
+# solutions will be better for different programs; see section 13 for the
+# specific requirements.
+#
+# You should also get your employer (if you work as a programmer) or school,
+# if any, to sign a "copyright disclaimer" for the program, if necessary.
+# For more information on this, and how to apply and follow the GNU AGPL, see
+# <http://www.gnu.org/licenses/>.
+"""TensorFlow 2.0 implementation of vanilla autoencoder"""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
