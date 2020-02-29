13812208154a3576283b8ca74afddd91e8c44ccc
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-03-01 01:52:16 +0800

    refactor: move module info

--- autoencoders/tf/autoencoder.py
+++ autoencoders/tf/autoencoder.py
@@ -33,11 +33,11 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-__version__ = "1.0.0"
-__author__ = "Abien Fred Agarap"
-
 from functools import partial
 import tensorflow as tf
+
+__author__ = "Abien Fred Agarap"
+__version__ = "1.0.0"
 
 
 dense = partial(
