3f031d32b710d46547257e39f494ab75b6d23702
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-03-11 12:26:20 +0800

    docs: add license header

--- autoencoders/pytorch/neural_network.py
+++ autoencoders/pytorch/neural_network.py
@@ -1,4 +1,23 @@
+# A Tutorial on Autoencoders
+# Copyright (C) 2019  Abien Fred Agarap and Richard Ralph Ricardo
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""PyTorch implementation of a feed-forward neural network"""
 import torch
+
+__author__ = "Abien Fred Agarap"
+__version__ = "1.0.0"
 
 
 class NeuralNet(torch.nn.Module):
