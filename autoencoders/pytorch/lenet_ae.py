0d05af48df75db59c0d35a6f8534af51da539f09
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-03-17 20:37:57 +0800

    refactor: remove unused imports

--- autoencoders/pytorch/lenet_ae.py
+++ autoencoders/pytorch/lenet_ae.py
@@ -33,15 +33,10 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import torch
+
 __author__ = "Abien Fred Agarap"
 __version__ = "1.0.0"
-
-import matplotlib.pyplot as plt
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import torchvision
 
 
 class LeNetAE(torch.nn.Module):
